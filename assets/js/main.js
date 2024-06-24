d699c2a234437970d59813a8a3297028d0316100
TusharGupta-Developer <tushar.developer127@gmail.com>
2024-06-24 11:45:35 +0530

    Setup Remove Menu in mobile when click happen on nav__link

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,8 @@ if (navToggle) {
     navToggle.addEventListener("click",()=>{
         navMenu.classList.add("show-menu")
     })
+//     The code first checks if the navToggle element exists.
+// If it exists, it adds an event listener for the click event.
 }
 
 // Menu hidden
@@ -17,3 +19,28 @@ if(navClose){
         navMenu.classList.remove("show-menu")
     })
 }
+
+// // We have also an option to use attribute Method like below 
+// if(navToggle){
+//     navToggle.addEventListener("click", ()=>{
+//         const currentClass = navMenu.getAttribute("class")
+//         navMenu.setAttribute("class"," show-menu")
+//     })
+// }
+
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+const navLink = document.querySelectorAll(".nav__link")
+
+const linkAction = ()=>{
+    const navMenu = document.getElementById("nav--menu")
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove("show-menu")
+}
+
+
+navLink.forEach(n => n.addEventListener("click", linkAction))
+// Purpose: Adds an event listener to each element in the navLink NodeList.
+
