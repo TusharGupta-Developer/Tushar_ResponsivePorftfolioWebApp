a39af82e2f25ea48e13a98bcd5c9e388f7f5fd83
TusharGupta-Developer <tushar.developer127@gmail.com>
2025-05-25 14:23:04 +0530

    Header & Main Buildup

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,7 +40,7 @@ const navLink = document.querySelectorAll(".nav__link")
 
 const linkAction = () => {
     const navMenu = document.getElementById("nav--menu")
-    // When we click on each nav__link, we remove the show-menu class
+    // When we click on each nav__link, we remove the show-menu class.
     navMenu.classList.remove("show-menu")
 }
 
